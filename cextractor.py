78e6aa0556b4abcdbbbcee27703290caca56789f
Kenji Hisazumi <kenji@hisazumi.jp>
2019-02-04 19:45:13 +0900

    added get_leaves

--- cextractor.py
+++ cextractor.py
@@ -11,20 +11,32 @@ class Node:
         self.type = type
         self.content = content
 
-    def set_child(self, child):
-        self.child = child
+    def set_children(self, children):
+        self.children = children
+
+    def p(self):
+        print("%s : %s" % (self.type, self.content))
 
     def pp(self, offset):
         print("%s%s: %s : %s" % (offset, self.level, self.type, self.content))
-        for child in self.child:
+        for child in self.children:
             child.pp(offset+' ')
+
+    def get_leaves(self):
+        if len(self.children) == 0:
+            return [self]
+        else:
+            leaves = []
+            for child in self.children:
+                leaves.extend(child.get_leaves())
+            return leaves
 
 def cindex2node(parent, level, current):
     node = Node(parent, level, current.kind.name, current.displayname)
     children = []
     for child in current.get_children():
         children.append(cindex2node(node, level+1, child))
-    node.set_child(children)
+    node.set_children(children)
     return node
 
 def function_name_split(identifier):
@@ -39,7 +51,10 @@ def function_name_split(identifier):
 def print_node_tree(node):
     for child in node.get_children():
         if child.kind.name == 'FUNCTION_DECL':
-            cindex2node(False, 0, child).pp('')
+            print("----")
+            tree = cindex2node(False, 0, child)
+            for leaf in tree.get_leaves():
+                leaf.p()
 
 if __name__ == "__main__":
     index = Index.create()
