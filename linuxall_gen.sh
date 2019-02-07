48b3651043064133a748762fbb9325ae386dec21
Kenji Hisazumi <kenji@hisazumi.jp>
2019-02-07 13:19:50 +0900

    added a script

---
+++ linuxall_gen.sh
@@ -0,0 +1,15 @@
+EXTCMD='python ../cextractor/cextractor.py'
+
+DIRS='crypto include lib scripts usr arch drivers   init     mm       security  virt block   firmware  ipc      net      sound certs   fs        kernel   samples  tools'
+
+mkdir -p data/linux
+
+for dir in $DIRS
+do
+    output=data/linux/$dir.txt
+    rm -f $output
+    for file in `find ../linux-4.20/$dir -name '*.c'`
+    do
+        $EXTCMD $file >> $output
+    done
+done
