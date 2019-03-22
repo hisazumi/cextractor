455af3b498098070a1b6d998949e8c3e93d9d1d8
Kenji Hisazumi <hisazumi@gmail.com>
2019-03-22 16:27:32 +0900

    rewrite shell calling code using $() instead of ``

--- linux-gen.sh
+++ linux-gen.sh
@@ -8,7 +8,7 @@ for dir in $DIRS
 do
     output=data/linux/raw/$dir.txt
     rm -f $output
-    for file in `find ../linux-4.20/$dir -name '*.c'`
+    for file in $(find ../linux-4.20/$dir -name '*.c')
     do
         $EXTCMD $file >> $output
     done
