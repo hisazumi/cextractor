3877e90822430333db20159872e8dda438015322
Kenji Hisazumi <kenji@hisazumi.jp>
2019-05-18 23:29:19 +0900

    fixed wrong use of argument

--- dictffmake.sh
+++ dictffmake.sh
@@ -2,4 +2,6 @@
 
 # generate filtered function names dictionary for each .c file
 DIR=$(find $1 -name '*.c')
-parallel --jobs 20 'sh -c "python -W ignore tfidf.py ../linux-4.20/total.fdict 26417 {}.fdict > {}.ffdict"' ::: $DIR
+CMD="python -W ignore tfidf.py $1/total.fdict 26417 {}.fdict > {}.ffdict"
+
+parallel --jobs 20 "sh -c $CMD" ::: $DIR
